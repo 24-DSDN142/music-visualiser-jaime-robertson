290fae377c901152a002ff5bb18de1bb453d241c
jaime-robertson <robertjaim1@myvuw.ac.nz>
2024-08-14 21:12:39 +1200

    Changed the default song to my song

--- music_starter.js
+++ music_starter.js
@@ -5,39 +5,58 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textFont('Verdana'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
+
+  let eyeSize = map(vocal,0,100,30,50);
+  fill(57,201,79);
+  ellipse(180,300,100,100);
+  ellipse(350,300,100,100);
+
+  ellipse(260,400,300,200);
+
+  fill(0);
+  ellipse(180,290,eyeSize,eyeSize);
+  ellipse(350,290,eyeSize,eyeSize);
+
+  let mouthSize = map(bass,0,100,20,150);
+  fill(0);
+  ellipse(260,430,200,mouthSize);
   
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
+}
+  
+
+
+//let bar_spacing = height / 10; {
+//    let bar_height = width / 12;
+//    let bar_pos_x = width / 2;
  
 
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+//    // vocal bar is red
+//    fill(200, 0, 0);
+//    rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+//    fill(0);
+//    text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+//    // drum bar is green
+//    fill(0, 200, 0);
+//    rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+//    fill(0);
+//    text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
+//    // bass bar is blue
+//    fill(50, 50, 240);
+//    rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
+//    fill(0);
+//    text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
  
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
+//    // other bar is white
+//    fill(200, 200, 200);
+//    rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
+//    fill(0);
+//    text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
+//    fill(255, 255, 0);
  
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
-}
+//    // display "words"
+//    textAlign(CENTER);
+//    textSize(vocal);
+//    text(words, width/2, height/3);
+// }
