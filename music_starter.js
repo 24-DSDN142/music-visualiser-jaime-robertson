0320a2ab9b5b6ed8b1937e1d7d956e320c50d7ba
jaime-robertson <robertjaim1@myvuw.ac.nz>
2024-09-08 22:14:26 +1200

    Updating timings
    Here I updated some timings so the red monster and eye only appeared after a certain sound effect on the song. Since I was able to do this, I also changed the colour of the window initially so it looked like the monster wasn't there yet. And I made the glass cracks disappear when the monster appeared.

--- music_starter.js
+++ music_starter.js
@@ -8,74 +8,110 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   textSize(24);
   console.log(counter);
 
+  angleMode(DEGREES);
+
   noStroke();
   
-  fill(230, 52, 32, 200);
+  if(counter < 1050){
+  fill(247, 235, 210, 200);
   rect(320,400,200,500);
   
-  fill(230,52,32,190);
+  fill(247, 235, 210, 190);
   rect(320,400,150,500)
 
   stroke(0);
-  fill(230, 52, 32, 220); // red
+  fill(247, 235, 210, 220); // red
   rect(320,400,250,500); // glass
   
   stroke(0);
-  fill(65); //grey
+  fill(59, 56, 56, 255); //grey
   
   rect(203,400,15,500); //left window frame
   rect(437,400,15,500); //right window frame
   rect(320,158,250,30); //top window frame 
 
-  rect(320,320,220,8) // middle window frame
+  rect(320,320,220,8); // middle window frame
 
-  rect(317,480,300,30)
+  rect(317,480,300,30);
 
   stroke(20); // black
   line(195,150,445,150);
 
-
- 
-    if(counter > 320){
-      line(210,230,245,232);
-      line(215,230,220,220);
-    
-      line(400,290,428,287);
-   }
+    if(counter > 300){
+    line(210,230,245,232);
+    line(215,230,220,220);
+  
+    line(400,290,428,287);
+    }
     else{ 
-       
-
-   }
- 
-
-   
-  let eyeSize = map(drum,0,100,20,80);
-  
-  fill(255);
-  ellipse(320,250,180,60);
-  
-  
-  fill(0);
-  ellipse(320,250,eyeSize,eyeSize);
-
-  fill(230,52,32,240);
-  rect(230,240,20,10)
-  
- 
-  //ellipse(180,300,100,100);
-  //ellipse(350,300,100,100);
-
-  //ellipse(260,400,300,200);
-
-  //fill(0);
-  //ellipse(180,290,eyeSize,eyeSize);
- 
-
-  //let mouthSize = map(bass,0,100,20,150);
-  //fill(0);
-  //ellipse(260,430,200,mouthSize);
+    
+    }
 
   }
+
+  else{
+
+    if(counter > 1150){  
+      fill(230, 52, 32, 200);
+      rect(320,400,200,500);
+      
+      fill(230,52,32,190);
+      rect(320,400,150,500)
+    
+      stroke(0);
+      fill(230, 52, 32, 220); // red
+      rect(320,400,250,500); // glass
+
+       
+      stroke(0);
+      fill(59, 56, 56, 255); //grey
+  
+      rect(203,400,15,500); //left window frame
+      rect(437,400,15,500); //right window frame
+      rect(320,158,250,30); //top window frame 
+
+      rect(320,320,220,8); // middle window frame
+
+      rect(317,480,300,30);
+
+      stroke(20); // black
+      line(195,150,445,150);
+      
+      let eyeSize = map(drum,0,100,10,70);
+  
+      fill(255);
+      ellipse(320,250,180,60);
+  
+  
+      fill(0);
+      ellipse(320,250,eyeSize,eyeSize);
+
+      fill(230,52,32,255);
+      noStroke()
+      rotate(-40);
+      rect(40,333,50,20);
+  
+      rotate(40);
+
+      stroke(0);
+      fill(0);
+      line(250,230,290,200)
+
+      let backStripe = map(bass,0,100,10,200)
+      noStroke();
+      fill(59, 56, 56,200);
+      rect(0,0,backStripe,1000);
+      rect(640,0,backStripe,1000);
+    }
+      else{
+
+
+    }
+
+
+  }
+
+}
 
 
   
